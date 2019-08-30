6719feb3f1fb2d997a7dc0329377d461cc8ec090
Rich Frost <richard@ipponsolutions.co.uk>
2019-08-30 13:08:13 +0100

    Fixed results for bulk upload

--- src/components/bulk-album-upload-results/index.js
+++ src/components/bulk-album-upload-results/index.js
@@ -8,17 +8,14 @@ export default class BulkAlbumUploadResults extends React.Component {
 
     if (results.length > 0) {
       return (
-        <>
-          <h2>Seals recorded, click on the links to view the seals images</h2>
-          <div className="card w-100">
-            {results.map(result => (
-              <>
-                <h3>{result.ppt}</h3>
-                <AlbumUploadResults results={result} />
-              </>
-            ))}
-          </div>
-        </>
+        <div className="card w-100">
+          {results.map(result => (
+            <>
+              <h3>{result.ppt}</h3>
+              <AlbumUploadResults results={result} />
+            </>
+          ))}
+        </div>
       );
     }
     return <div />;
