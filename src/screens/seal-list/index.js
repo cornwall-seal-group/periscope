424369c4459de6e6283afe1f17aeb9bbf826e5ac
Rich Frost <richard@ipponsolutions.co.uk>
2019-09-09 17:03:51 +0100

    Added filtering by tag on seal list page

--- src/screens/seal-list/index.js
+++ src/screens/seal-list/index.js
@@ -54,6 +54,31 @@ class SealList extends Component {
     });
   }
 
+  setTags = tags => {
+    tags.sort(function(a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
+    tags.map(tag => {
+      const split = tag.name.split("-");
+
+      tag.name =
+        split[0].toUpperCase() +
+        " " +
+        split[1] +
+        (split[2] ? " " + split[2].toUpperCase() : "");
+      return tag;
+    });
+
+    return tags;
+  };
+
   onFilter = e => {
     this.setState({ filter: e.target.value });
   };
